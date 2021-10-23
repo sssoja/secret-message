c96ec8ffe7bc78fce462101ea326340f2eb5cc8d
Sophie <sophie@sssoja.com>
2021-10-23 19:43:33 +0100

    include secret message

--- app.js
+++ app.js
@@ -38,3 +38,6 @@ secretMessage.unshift("Programming");
 secretMessage.splice(6, 3, "know");
 
 console.log(secretMessage.join(" "));
+
+/* Should print "Programming is not about what you know first time, 
+it is about what you can figure out. -2015, Chris Pine, Learn to Program" */
